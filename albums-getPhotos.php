685a001e5d3594582ddef957dc3ff63a719fb672
Kim LaRocca <kim@kimlarocca.com>
2020-05-02 11:33:59 -0400

    ugh

--- albums-getPhotos.php
+++ albums-getPhotos.php
@@ -4,13 +4,8 @@ session_start();
 ?>
 <?php require_once('Connections/cms.php'); ?>
 <?php
-$host = $hostname_cms;
-$user = $username_cms;
-$pass = $password_cms;
-$databaseName = $database_cms;
-$con = mysqli_connect($host, $user, $pass, $databaseName);
-$dbs = mysqli_select_db($con, $databaseName);
-$result = mysqli_query($con, "SELECT * FROM photos WHERE albumID = " . $_GET['albumID'] . " AND id >= " . $_GET['lastID'] . " ORDER BY id");
+mysqli_select_db($cms, $database_cms);
+$result = mysqli_query($cms, "SELECT * FROM photos WHERE albumID = " . $_GET['albumID'] . " AND id >= " . $_GET['lastID'] . " ORDER BY id");
 $data = array();
 while ($row = mysqli_fetch_row($result)) {
     $data[] = $row;
