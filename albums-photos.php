8f1db5b94d300832fbecf6012b46ee7dc58345c5
Kim LaRocca <kim@kimlarocca.com>
2020-05-02 11:06:40 -0400

    bs

--- albums-photos.php
+++ albums-photos.php
@@ -14,40 +14,40 @@ $MM_authorizedUsers = "";
 $MM_donotCheckaccess = "true";
 
 // *** Restrict Access To Page: Grant or deny access to this page
-function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
-  // For security, start by assuming the visitor is NOT authorized. 
-  $isValid = False; 
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
+  // For security, start by assuming the visitor is NOT authorized.
+  $isValid = False;
 
-  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
-  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
-  if (!empty($UserName)) { 
-    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
-    // Parse the strings into arrays. 
-    $arrUsers = Explode(",", $strUsers); 
-    $arrGroups = Explode(",", $strGroups); 
-    if (in_array($UserName, $arrUsers)) { 
-      $isValid = true; 
-    } 
-    // Or, you may restrict access to only certain users based on their username. 
-    if (in_array($UserGroup, $arrGroups)) { 
-      $isValid = true; 
-    } 
-    if (($strUsers == "") && true) { 
-      $isValid = true; 
-    } 
-  } 
-  return $isValid; 
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username.
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank.
+  if (!empty($UserName)) {
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login.
+    // Parse the strings into arrays.
+    $arrUsers = Explode(",", $strUsers);
+    $arrGroups = Explode(",", $strGroups);
+    if (in_array($UserName, $arrUsers)) {
+      $isValid = true;
+    }
+    // Or, you may restrict access to only certain users based on their username.
+    if (in_array($UserGroup, $arrGroups)) {
+      $isValid = true;
+    }
+    if (($strUsers == "") && true) {
+      $isValid = true;
+    }
+  }
+  return $isValid;
 }
 
 $MM_restrictGoTo = "index.php?action=failed";
-if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {
   $MM_qsChar = "?";
   $MM_referrer = $_SERVER['PHP_SELF'];
   if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
-  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0)
   $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
   $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
-  header("Location: ". $MM_restrictGoTo); 
+  header("Location: ". $MM_restrictGoTo);
   exit;
 }
 ?>
@@ -62,34 +62,39 @@ if ($_GET['action'] == 'cover') {
 ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+    {
+        if (PHP_VERSION < 6) {
+            $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+        }
+        $hostname_cms = "localhost";
+        $database_cms = "kim_4site";
+        $username_cms = "kim_larocca";
+        $password_cms = "Lotus18641864!";
+        $cms = mysqli_connect($hostname_cms, $username_cms, $password_cms, $database_cms) or trigger_error(mysqli_error($cms),E_USER_ERROR);
 
-  $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($theValue) : mysqli_escape_string($theValue);
+        $theValue = mysqli_real_escape_string($cms, $theValue);
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
+        switch ($theType) {
+            case "text":
+                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+                break;
+            case "long":
+            case "int":
+                $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+                break;
+            case "double":
+                $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+                break;
+            case "date":
+                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+                break;
+            case "defined":
+                $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+                break;
+        }
+        return $theValue;
+    }
 }
 
 $editFormAction = $_SERVER['PHP_SELF'];
@@ -103,7 +108,7 @@ if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
                        GetSQLValueString($_POST['albumID'], "int"));
 
   mysqli_select_db($cms, $database_cms);
-  $Result1 = mysqli_query($updateSQL, $cms) or die(mysqli_error($cms));
+  $Result1 = mysqli_query($cms, $updateSQL) or die(mysqli_error($cms));
 
   $updateGoTo = "?action=saved&albumID=".$_POST['albumID'];
   //if (isset($_SERVER['QUERY_STRING'])) {
@@ -119,7 +124,7 @@ if (isset($_SESSION['MM_Username'])) {
 }
 mysqli_select_db($cms, $database_cms);
 $query_currentUser = sprintf("SELECT * FROM cmsUsers WHERE username = %s", GetSQLValueString($colname_currentUser, "text"));
-$currentUser = mysqli_query($query_currentUser, $cms) or die(mysqli_error($cms));
+$currentUser = mysqli_query($cms, $query_currentUser) or die(mysqli_error($cms));
 $row_currentUser = mysqli_fetch_assoc($currentUser);
 $totalRows_currentUser = mysqli_num_rows($currentUser);
 
@@ -129,13 +134,13 @@ if (isset($_GET['albumID'])) {
 }
 mysqli_select_db($cms, $database_cms);
 $query_album = sprintf("SELECT * FROM photoAlbums WHERE albumID = %s", GetSQLValueString($colname_album, "int"));
-$album = mysqli_query($query_album, $cms) or die(mysqli_error($cms));
+$album = mysqli_query($cms, $query_album) or die(mysqli_error($cms));
 $row_album = mysqli_fetch_assoc($album);
 $totalRows_album = mysqli_num_rows($album);
 
 mysqli_select_db($cms, $database_cms);
 $query_photos = "SELECT * FROM photos WHERE albumID = ".$_GET['albumID']." ORDER BY photoSequence ASC";
-$photos = mysqli_query($query_photos, $cms) or die(mysqli_error($cms));
+$photos = mysqli_query($cms, $query_photos) or die(mysqli_error($cms));
 $row_photos = mysqli_fetch_assoc($photos);
 $totalRows_photos = mysqli_num_rows($photos);
 ?>
@@ -191,8 +196,8 @@ if ($_GET['action'] == 'cover') print '<p style="color:red;">Album cover photo b
 		<!--
         <div class="pin"><img src="http://placehold.it/260x250">
           <h2>subtitle</h2>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Sed feugiat consectetur pellentesque. Nam ac elit risus, 
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed feugiat consectetur pellentesque. Nam ac elit risus,
             ac blandit dui. Duis rutrum porta tortor ut convallis.
             Duis rutrum porta tortor ut convallis. </p>
         </div>
@@ -204,12 +209,12 @@ if ($_GET['action'] == 'cover') print '<p style="color:red;">Album cover photo b
 var lastID = 0;
 setInterval(ajaxCall, 1000);
 function ajaxCall() {
- $.ajax({                                      
-  url: 'albums-getPhotos.php?albumID=<?php echo $_GET['albumID'] ?>&lastID='+lastID, data: "", dataType: 'json',  success: function(rows)        
+ $.ajax({
+  url: 'albums-getPhotos.php?albumID=<?php echo $_GET['albumID'] ?>&lastID='+lastID, data: "", dataType: 'json',  success: function(rows)
   {
     for (var i in rows)
     {
-      var row = rows[i];          
+      var row = rows[i];
       var id = row[0];
       var file_name = row[1];
       var description = row[6];
@@ -224,9 +229,9 @@ function ajaxCall() {
 		if(photoTitle == null) { photoTitle = ''; }
 		  $('#photos').append('<div class="pin"><a href="photos-modify.php?albumID=<?php echo $row_album['albumID']; ?>&photoID='+id+'"><img src="uploads/thumb-'+file_name+'"/></a><h2>'+photoTitle+'</h2><p>'+description+'</p><div style="margin:auto; width:100px; padding:10px 0 10px 0"><a href="albums-photos.php?action=cover&albumID=<?php echo $row_album['albumID']; ?>&photoID='+id+'" class="tooltip" title="set album cover"><img style="width:22px" src="images/'+coverPhoto+'.png" width="32" height="32" /></a> <a href="photos-modify.php?albumID=<?php echo $row_album['albumID']; ?>&photoID='+id+'" class="tooltip" title="update photo"><img style="width:22px" src="images/edit.png" width="32" height="32" /></a> <a href="photos-delete.php?albumID=<?php echo $row_album['albumID']; ?>&photoID='+id+'" class="tooltip" title="delete photo"><img style="width:22px" src="images/delete.png" width="32" height="32" /></a></div></div>');
 	  }
-    } 
+    }
 	if (id != null) {lastID = id;}
-  } 
+  }
 });
 }
 </script>
--- albums.php
+++ albums.php
@@ -11,73 +11,78 @@ $MM_authorizedUsers = "";
 $MM_donotCheckaccess = "true";
 
 // *** Restrict Access To Page: Grant or deny access to this page
-function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
-  // For security, start by assuming the visitor is NOT authorized. 
-  $isValid = False; 
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
+  // For security, start by assuming the visitor is NOT authorized.
+  $isValid = False;
 
-  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
-  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
-  if (!empty($UserName)) { 
-    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
-    // Parse the strings into arrays. 
-    $arrUsers = Explode(",", $strUsers); 
-    $arrGroups = Explode(",", $strGroups); 
-    if (in_array($UserName, $arrUsers)) { 
-      $isValid = true; 
-    } 
-    // Or, you may restrict access to only certain users based on their username. 
-    if (in_array($UserGroup, $arrGroups)) { 
-      $isValid = true; 
-    } 
-    if (($strUsers == "") && true) { 
-      $isValid = true; 
-    } 
-  } 
-  return $isValid; 
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username.
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank.
+  if (!empty($UserName)) {
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login.
+    // Parse the strings into arrays.
+    $arrUsers = Explode(",", $strUsers);
+    $arrGroups = Explode(",", $strGroups);
+    if (in_array($UserName, $arrUsers)) {
+      $isValid = true;
+    }
+    // Or, you may restrict access to only certain users based on their username.
+    if (in_array($UserGroup, $arrGroups)) {
+      $isValid = true;
+    }
+    if (($strUsers == "") && true) {
+      $isValid = true;
+    }
+  }
+  return $isValid;
 }
 
 $MM_restrictGoTo = "index.php?action=failed";
-if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {
   $MM_qsChar = "?";
   $MM_referrer = $_SERVER['PHP_SELF'];
   if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
-  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0)
   $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
   $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
-  header("Location: ". $MM_restrictGoTo); 
+  header("Location: ". $MM_restrictGoTo);
   exit;
 }
 ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+    {
+        if (PHP_VERSION < 6) {
+            $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+        }
+        $hostname_cms = "localhost";
+        $database_cms = "kim_4site";
+        $username_cms = "kim_larocca";
+        $password_cms = "Lotus18641864!";
+        $cms = mysqli_connect($hostname_cms, $username_cms, $password_cms, $database_cms) or trigger_error(mysqli_error($cms),E_USER_ERROR);
 
-  $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($theValue) : mysqli_escape_string($theValue);
+        $theValue = mysqli_real_escape_string($cms, $theValue);
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
+        switch ($theType) {
+            case "text":
+                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+                break;
+            case "long":
+            case "int":
+                $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+                break;
+            case "double":
+                $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+                break;
+            case "date":
+                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+                break;
+            case "defined":
+                $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+                break;
+        }
+        return $theValue;
+    }
 }
 
 $editFormAction = $_SERVER['PHP_SELF'];
@@ -107,13 +112,13 @@ if (isset($_SESSION['MM_Username'])) {
 }
 mysqli_select_db($cms, $database_cms);
 $query_currentUser = sprintf("SELECT * FROM cmsUsers WHERE username = %s", GetSQLValueString($colname_currentUser, "text"));
-$currentUser = mysqli_query($query_currentUser, $cms) or die(mysqli_error($cms));
+$currentUser = mysqli_query($cms, $query_currentUser) or die(mysqli_error($cms));
 $row_currentUser = mysqli_fetch_assoc($currentUser);
 $totalRows_currentUser = mysqli_num_rows($currentUser);
 
 mysqli_select_db($cms, $database_cms);
 $query_albums = "SELECT * FROM photoAlbums LEFT JOIN (SELECT photos.id,photos.file_name FROM photos) AS a ON photoAlbums.coverPhotoID=a.id WHERE photoAlbums.websiteID = ".$row_currentUser['websiteID']." ORDER BY albumName";
-$albums = mysqli_query($query_albums, $cms) or die(mysqli_error($cms));
+$albums = mysqli_query($cms, $query_albums) or die(mysqli_error($cms));
 $row_albums = mysqli_fetch_assoc($albums);
 $totalRows_albums = mysqli_num_rows($albums);
 ?>
@@ -148,22 +153,22 @@ if ($_GET['action'] == 'deleted') print '<p style="color:red;">Album has been de
         <input type="hidden" name="MM_insert" value="form1" />
       </form>
       <h3>manage photo albums</h3>
-        <?php 
+        <?php
 		if ($totalRows_albums < 1) echo '<p>You do not have any photo albums.</p>';
 		else {
 			?>
 			<table>
 			<?php
-            do { 
+            do {
 			$albumID = $row_albums['albumID'];
 			?><tr>
             <td style="width:200px;">
-            <?php  
-		if ($row_albums['coverPhotoID'] == NULL){ 
+            <?php
+		if ($row_albums['coverPhotoID'] == NULL){
 		?>
         <a href="albums-photos.php?albumID=<?php echo $albumID ?>"><img src="uploads/comingsoon.jpg"></a>
         <?php
-		} else {			
+		} else {
 		?>
         <a href="albums-photos.php?albumID=<?php echo $albumID; ?>"><img src="uploads/<?php echo $row_albums['file_name']; ?>"></a>
         <?php
