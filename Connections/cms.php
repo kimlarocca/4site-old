20bdbf2b1bd63cf00bba62b7750d983909dfa9e1
Kim LaRocca <kim@kimlarocca.com>
2020-05-02 11:55:37 -0400

    bs

--- Connections/cms.php
+++ Connections/cms.php
@@ -9,10 +9,4 @@ $database_cms = "kim_4site";
 $username_cms = "kim_larocca";
 $password_cms = "Lotus18641864!";
 $cms = mysqli_connect($hostname_cms, $username_cms, $password_cms, $database_cms) or trigger_error(mysqli_error($cms),E_USER_ERROR);
-
-function console_log( $data ){
-    echo '<script>';
-    echo 'console.log('. json_encode( $data ) .')';
-    echo '</script>';
-}
 ?>
