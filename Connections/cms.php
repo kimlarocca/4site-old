1f3412735f7c0051aac52da2b8b281983b0181f1
Kim LaRocca <kim@kimlarocca.com>
2020-05-10 22:03:00 -0400

    removing errors

--- Connections/cms.php
+++ Connections/cms.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
