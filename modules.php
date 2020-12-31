087f913c4891abb852b86921a65394a6954da922
Kim LaRocca <kim@kimlarocca.com>
2020-12-31 16:47:58 -0500

    fix mysqli errors

--- modules.php
+++ modules.php
@@ -1,83 +1,88 @@
 <?php
-ini_set('session.save_path',getcwd(). '/../tmp/');
-session_start();
+//ini_set('session.save_path',getcwd(). '/../tmp/');
+//session_start();
 ?>
 <?php require_once('Connections/cms.php'); ?>
 <?php
-if (!isset($_SESSION)) {
-  session_start();
-}
-$MM_authorizedUsers = "";
-$MM_donotCheckaccess = "true";
-
-// *** Restrict Access To Page: Grant or deny access to this page
-function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
-  // For security, start by assuming the visitor is NOT authorized.
-  $isValid = False;
-
-  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username.
-  // Therefore, we know that a user is NOT logged in if that Session variable is blank.
-  if (!empty($UserName)) {
-    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login.
-    // Parse the strings into arrays.
-    $arrUsers = Explode(",", $strUsers);
-    $arrGroups = Explode(",", $strGroups);
-    if (in_array($UserName, $arrUsers)) {
-      $isValid = true;
-    }
-    // Or, you may restrict access to only certain users based on their username.
-    if (in_array($UserGroup, $arrGroups)) {
-      $isValid = true;
-    }
-    if (($strUsers == "") && true) {
-      $isValid = true;
-    }
-  }
-  return $isValid;
-}
-
-$MM_restrictGoTo = "index.php?action=failed";
-if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {
-  $MM_qsChar = "?";
-  $MM_referrer = $_SERVER['PHP_SELF'];
-  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
-  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0)
-  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
-  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
-  header("Location: ". $MM_restrictGoTo);
-  exit;
-}
-?>
+//if (!isset($_SESSION)) {
+//  session_start();
+//}
+//$MM_authorizedUsers = "";
+//$MM_donotCheckaccess = "true";
+//
+//// *** Restrict Access To Page: Grant or deny access to this page
+//function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
+//  // For security, start by assuming the visitor is NOT authorized.
+//  $isValid = False;
+//
+//  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username.
+//  // Therefore, we know that a user is NOT logged in if that Session variable is blank.
+//  if (!empty($UserName)) {
+//    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login.
+//    // Parse the strings into arrays.
+//    $arrUsers = Explode(",", $strUsers);
+//    $arrGroups = Explode(",", $strGroups);
+//    if (in_array($UserName, $arrUsers)) {
+//      $isValid = true;
+//    }
+//    // Or, you may restrict access to only certain users based on their username.
+//    if (in_array($UserGroup, $arrGroups)) {
+//      $isValid = true;
+//    }
+//    if (($strUsers == "") && true) {
+//      $isValid = true;
+//    }
+//  }
+//  return $isValid;
+//}
+//
+//$MM_restrictGoTo = "index.php?action=failed";
+//if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {
+//  $MM_qsChar = "?";
+//  $MM_referrer = $_SERVER['PHP_SELF'];
+//  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+//  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0)
+//  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+//  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+//  header("Location: ". $MM_restrictGoTo);
+//  exit;
+//}
+//?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+    {
+        if (PHP_VERSION < 6) {
+            $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+        }
+        $hostname_cms = "localhost";
+        $database_cms = "kim_4site";
+        $username_cms = "kim_larocca";
+        $password_cms = "Lotus18641864!";
+        $cms = mysqli_connect($hostname_cms, $username_cms, $password_cms, $database_cms) or trigger_error(mysqli_error($cms), E_USER_ERROR);
 
-  $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($theValue) : mysqli_escape_string($theValue);
+        $theValue = mysqli_real_escape_string($cms, $theValue);
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
+        switch ($theType) {
+            case "text":
+                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+                break;
+            case "long":
+            case "int":
+                $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+                break;
+            case "double":
+                $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+                break;
+            case "date":
+                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+                break;
+            case "defined":
+                $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+                break;
+        }
+        return $theValue;
+    }
 }
 
 $editFormAction = $_SERVER['PHP_SELF'];
@@ -129,25 +134,25 @@ $totalRows_currentUser = mysqli_num_rows($currentUser);
 
 mysqli_select_db($cms, $database_cms);
 $query_modules = "SELECT * FROM cmsModules ORDER BY moduleName ASC";
-$modules = mysqli_query($query_modules, $cms) or die(mysqli_error($cms));
+$modules = mysqli_query($cms, $query_modules) or die(mysqli_error($cms));
 $row_modules = mysqli_fetch_assoc($modules);
 $totalRows_modules = mysqli_num_rows($modules);
 
 mysqli_select_db($cms, $database_cms);
 $query_userModules = "SELECT * FROM cmsUserModules,cmsWebsites,cmsModules WHERE cmsUserModules.websiteID=cmsWebsites.websiteID AND cmsUserModules.moduleID=cmsModules.moduleID";
-$userModules = mysqli_query($query_userModules, $cms) or die(mysqli_error($cms));
+$userModules = mysqli_query($cms, $query_userModules) or die(mysqli_error($cms));
 $row_userModules = mysqli_fetch_assoc($userModules);
 $totalRows_userModules = mysqli_num_rows($userModules);
 
 mysqli_select_db($cms, $database_cms);
 $query_websites = "SELECT websiteID, url FROM cmsWebsites ORDER BY url ASC";
-$websites = mysqli_query($query_websites, $cms) or die(mysqli_error($cms));
+$websites = mysqli_query($cms, $query_websites) or die(mysqli_error($cms));
 $row_websites = mysqli_fetch_assoc($websites);
 $totalRows_websites = mysqli_num_rows($websites);
 
 mysqli_select_db($cms, $database_cms);
 $query_modules2 = "SELECT * FROM cmsModules ORDER BY moduleName ASC";
-$modules2 = mysqli_query($query_modules2, $cms) or die(mysqli_error($cms));
+$modules2 = mysqli_query($cms, $query_modules2) or die(mysqli_error($cms));
 $row_modules2 = mysqli_fetch_assoc($modules2);
 $totalRows_modules2 = mysqli_num_rows($modules2);
 ?>
